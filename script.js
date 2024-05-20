7a3e1eca57bffcfd1f5b354fb72f97ad16efc972
Muhammad Aman Siddiqui <154512222+M-AmanSiddiqui@users.noreply.github.com>
2024-05-21 02:31:26 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Error! Please enter a valid Password.")
+var name = "Welcome to Js Landing...."
+var second = " Happy Coding"
+alert(name+ "\n" + second)
+alert("Welcome to Js Land")
+var third ="Happy Coding!" 
+var four = "Prevent This Page From Creating Additional Dialoges"
+alert(third+ "\n" +four)
+alert("Hello...I can run js through my web browser's console")
+
